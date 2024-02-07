f08fdd6797e9e3032768c548784353ec88da747a
Mariam Senzia <mariamsenzia@gmail.com>
2024-02-07 19:30:47 +0300

    implemented react context

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { UserContext } from "../context/user";
 import Interests from "./Interests";
+import { useContext } from "react";
 
-function Profile({ user, theme }) {
+function Profile({ theme }) {
+  const user = useContext(UserContext);
+  console.log(user)
+
   if (!user) return <h2>Please Login To View Profile</h2>;
   return (
     <div>
